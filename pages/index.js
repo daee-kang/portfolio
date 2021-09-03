ce5c1559717348a7589e269a68be302e4aaf4c13
daee <daee@unlv.nevada.edu>
2021-09-03 16:14:14 -0700

    idk add some seo stuff

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,11 @@ export default function Home() {
     <div>
       <Head>
         <title>Daee Kang | Portfolio</title>
-        <meta name="description" content="Daee Kang Porfolio" />
+        <meta name="description" content="Daee Kang Portfolio" />
+        <meta property="og:title" content="Daee Kang Portfolio" />
+        <meta property="og:description" content="Daee Kang Portfolio" />
+        <meta property="og:url" content="http://daee.dev/" />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preload" href="/fonts/apercuMono.ttf" as="font" crossOrigin="" />
       </Head>
